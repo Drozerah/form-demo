6c2410165a94dc2c0d0f512df3273ef326f23bea
Drozerah <drozerah@gmail.com>
2019-08-05 20:03:18 +0200

    refactor: form validation and submission
    - validate form:
    	- check if all inputs/texterea are valid then activate/disable the submit button
    - textarea events management:
    	- switch icon color according to text length and validity
    	- show/hide error message
    - form submission:
    	- disable enter key on form except textarea
    	- ask for confirmation befor submission
    	- reset the form after submission
    - add better JSdoc info
    - rename some services

--- src/app/utils/form.service.js
+++ src/app/utils/form.service.js
@@ -22,7 +22,6 @@ const initSelect = () => {
             return M.FormSelect.init(elems, options)
         })
     }
-
 }
 
 /**
@@ -40,73 +39,242 @@ const initSelect = () => {
  * 
  * @author Drozerah https://github.com/Drozerah
  */
-const initCharacterCounter = id => {
+const initCharacterCounter = name => {
 
     document.addEventListener('DOMContentLoaded', () => {
 
-        if(document.getElementById(id)){
+        if(document.getElementById(name)){
 
-            const elem = document.getElementById(id)
+            const elem = document.getElementById(name)
             return  M.CharacterCounter.init(elem)
         }
     })
 }
 
 /**
- * Return an alert when the form is submitted then reset the form
+ * Check if all inputs/texterea are valid then activate/disable the submit button
  * 
- * - note: the function currently disable the form's default action
+ * - if an input or the textarea is not valid then disable the form submit button
+ * - else if all elements are valid then activate the submit button
+ * - we use the MutationObserver interface to observe the form class mutations
  * 
- * @param string the HTML markup id selector
- * @param boolean reset the form if true
- * @returns JS alert prompt and the form reset
+ * @doc https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver/MutationObserver
  * 
- * @author Drozerah https://github.com/Drozerah
+ * @param string the attribute name value
  */
-const submitForm = (id, isReset ) => {
+const validateForm = name => {
+    
+    const form = document.forms[name]
+    const btn = form.submit
+    const validateElems = document.getElementsByClassName("validate")
 
-    if(document.getElementById(id)){
+    // convert inputs collection to a type array
+    const array = Array.from(validateElems)
 
-        const form = document.getElementById(id)
+    // push textarea into the array
+    array.push(form.textarea)
 
-        form.addEventListener('submit', e => {
+    /**
+     * MUTATION OBSERVER
+     */
 
-            e.preventDefault()
+    // Select the node that will be observed for mutations
+    const targetNode = form
 
-            alert('Submitted !')
-            
-            if(isReset){
-                e.target.reset() // reset after alert   
-            }
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: true, childList: true, subtree: true }
 
-        }, false)
+    // Callback function to execute when mutations are observed
+    const callback = mutationsList => {
+
+        for(let mutation of mutationsList) {
+
+            if(mutation.target.name && mutation.target.name.match(/^(name|email|age|textarea)$/)){
+        
+                // console.log( // ~~DEV-DEBUG
+                //     `=> ${mutationsID}
+                //     => new mutation\n
+                //     => target : ${mutation.target}
+                //     => target.name : ${mutation.target.name}
+                //     => target.name.classList : ${mutation.target.classList.value}
+                //     => areValid ? ${array.every( elem => elem.classList[1] != "invalid")}`
+                // )
+
+                if(array.every( elem => elem.classList[1] != "invalid")){
+
+                    btn.disabled = false // true condition => the submit is not disabled
+                }else{ //false
+
+                    btn.disabled = true // else, the submit is disabled
+                }
+            }           
+        }
     }
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback)
+
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config)
 }
 
 /**
- *  Toogle textarea icon color
  * 
- *  - Change the textarea icon color based on the <textarea></textarea> focus & blur events 
+ * Textarea events management
  * 
- * @param string the HTML textarea markup id selector
- * @param string the HTML icon markup id selector
+ * - switch textarea icon color according to text length
+ * - switch textarea icon color according to validity
+ * - show/hide error message
+ * 
+ * @param string the HTML markup id selector
  * 
  * @author Drozerah https://github.com/Drozerah
  */
-const toogleIconColor = (id, iconId) => {
+const validateFormMessage = name => {
+
+    const textarea = document.forms[name].textarea
+    const colorRed = '#F44336'
+    const primaryColor = '#00e0a5'
+    const icon = document.getElementById('textarea-icon')
+    const errorElm = document.getElementById('textarea-error')
+    const errorMsg = "You have reached the maximum number of characters!"
     
-    if(document.getElementById(id) && document.getElementById(iconId)){
+    textarea.addEventListener("input", e => {
 
-        const elem = document.getElementById(id)
-        const icon = document.getElementById(iconId)
+        const target = e.target
+        const maxLength = target.dataset.length
+        const currentLength = target.value.length
 
-        elem.addEventListener('focus', () => {
-            icon.style.color = '#00e0a5'   
-        })
-        elem.addEventListener('blur', () => {
-            icon.style.color = 'inherit'   
-        })
-    }
+        if (currentLength > maxLength) {
+            errorElm.innerText = errorMsg
+            errorElm.style.display = 'inline' // show
+            icon.style.color = colorRed // red
+        }else{
+            errorElm.style.display = 'none' // hide
+            errorElm.innerText = "" // empty text
+            icon.style.color = primaryColor // primary color
+        }
+    })
+  
+    textarea.addEventListener('focus', e => {
+            
+        if(e.target.classList[1]){ // has class invalid
+            icon.style.color = colorRed // red 
+        }else{
+            icon.style.color = primaryColor // primary color  
+        }
+    })
+
+    textarea.addEventListener('blur', e => {
+
+        if(e.target.classList[1]){ // has class invalid
+            icon.style.color = colorRed // red color 
+        }else{
+            icon.style.color = 'inherit' // default color
+        } 
+    })
+}
+
+/**
+ * Form Submission 
+ * 
+ * - disable enter key on form except textarea
+ * - ask confirmation befor submission
+ * - reset the form after submission 
+ * 
+ * @params string the attribute name value
+ * @author Drozerah https://github.com/Drozerah
+ */
+const submitForm = name => {
+
+    const form = document.forms[name]
+
+    /**
+     * Disable Enter Key On Form except Textarea
+     */
+    form.addEventListener('keypress', (event) => {
+
+        if (event.which == 13 && event.target.nodeName  != "TEXTAREA") {
+            event.preventDefault()
+            return false
+        }
+    })
+
+    /**
+     * Form Submission 
+     */
+    form.addEventListener('submit', (event) => {
+
+        event.preventDefault()
+
+// console.log('===========')
+// console.log(form.elements)
+// console.log('===========')
+
+        /**
+         * CONFIRMATION
+         */
+
+        confirm("Confirmation?")
+
+        /** 
+         * WORKING WITH FORM DATA
+        */
+
+// const result = `
+// name: ${form.elements.name.name}\n
+// value: ${form.elements.name.value}`
+
+// alert(result)
+
+//         // form data object
+//         const formData = new FormData(form)
+//         // get all form elements
+//         console.log(form.elements)
+//         // get first form element
+//         console.log(form.elements[0])
+//         // get first form element value
+//         console.log(`value: ${form.elements[0].value}`)
+//         // get first form element name
+//         console.log(`name attribut: ${form.elements[0].id}`)
+//         console.log(`name attribut: ${form.elements[0].name}`)
+
+//         // FormData.append()
+//         // appends a new value onto an existing key inside
+//         // @{mdn} https://developer.mozilla.org/en-US/docs/Web/API/FormData/append
+//         formData.append(form.elements[0].name, form.elements[0].value)
+
+//         // FormData.get()
+//         // returns the first value associated with a given key from within a FormData object
+//         // @{mdn} https://developer.mozilla.org/en-US/docs/Web/API/FormData/get
+//         console.log(formData.get(form.elements[0].name))
+
+//         // FormData.entries()
+//         // going through all key/value pairs contained in the object
+//         // @{mdn} https://developer.mozilla.org/fr/docs/Web/API/FormData/entries 
+//         for(var pair of formData.entries()) {
+//             console.log(pair[0]+ ', '+ pair[1]); 
+//         }
+
+
+        /**
+         * FORM RESET
+        */
+
+        // remove focus state on any input fields
+        const inputs = document.querySelectorAll('input')
+        inputs.forEach( input => input.blur())
+
+        // resize textarea height
+        form.textarea.style.height = "21px"
+    
+        // remove existing Character Counter inner Text
+        const counterEl = form.textarea.M_CharacterCounter.counterEl
+        counterEl.innerText = ""
+        
+        // reset the form
+        form.reset()
+    })   
 }
 
 /**
@@ -114,7 +282,8 @@ const toogleIconColor = (id, iconId) => {
  */
 export { 
     initSelect,
-    submitForm,
     initCharacterCounter,
-    toogleIconColor
+    validateForm,
+    validateFormMessage,
+    submitForm
 }
